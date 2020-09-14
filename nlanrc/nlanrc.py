24806f3b76b1f4401bbad2e4213e27209e318ed0
Tanner Purves <tpurves@kickbackpoints.com>
2020-09-14 10:17:19 -0600

    Fix logging format.

--- nlanrc/nlanrc.py
+++ nlanrc/nlanrc.py
@@ -33,7 +33,7 @@ def main():
 
     handlers = [StreamHandler()]
     log = logging.getLogger(__name__)
-    logging.basicConfig(level=level, format="%(asctime)s %(name)-12s: %(levelname)-8s %(message)s", handlers=handlers)
+    logging.basicConfig(level=level, format="%(asctime)s %(name)-15s: %(levelname)-8s %(message)s", handlers=handlers)
 
     port = arguments.get("--port")
 
